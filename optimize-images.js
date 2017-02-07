fde373f78c8c1e8c96f2c37f4436483d1b4d39bd
Niels Gerritsen <n.c.gerritsen@gmail.com>
2017-02-07 16:42:24 +0100

    Optimize images afterwards

--- optimize-images.js
+++ optimize-images.js
@@ -3,21 +3,23 @@ const path = require('path')
 const imagemin = require('imagemin')
 const imageminMozjpeg = require('imagemin-mozjpeg')
 const imageminPngquant = require('imagemin-pngquant')
+const recursiveFs = require('recursive-fs')
 
-const POST_SOURCE_FOLDER = './source/_posts'
-const IMAGE_GLOB = '*.{jpg,png}'
+const PUBLIC_FOLDER = './public'
+const IMAGE_EXTENSIONS = ['.jpg', '.png']
 
-fs.readdirSync(POST_SOURCE_FOLDER)
-  .map(filename => path.join(POST_SOURCE_FOLDER, filename))
-  .filter(filepath => fs.statSync(filepath).isDirectory())
-  .forEach(filepath => {
-    imagemin([path.join(filepath, IMAGE_GLOB)], filepath, {
-      plugins: [
-        imageminMozjpeg(),
-        imageminPngquant({quality: '65-80'})
-      ]
-    })
-      .then(files => {
-        files.forEach(file => console.log(`Processed image "${file.path}"`))
+recursiveFs.readdirr('./public', (err, dirs, files) => {
+  const filepaths = files
+    .filter(file => IMAGE_EXTENSIONS.includes(path.extname(file)))
+    .forEach(filepath => {
+      imagemin([filepath], path.dirname(filepath), {
+        plugins: [
+          imageminMozjpeg(),
+          imageminPngquant({ quality: '65-80' })
+        ]
       })
-  })
+        .then(files => {
+          files.forEach(file => console.log(`Optimized image "${file.path}"`))
+        })
+    })
+})
